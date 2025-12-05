d4132ddfaf14cbb272c9c5dff6e28ddce239c691
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-05 18:38:14 +0200

    Time: 3 ms (98.55%), Space: 10.4 MB (86.2%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<unsigned long> dp(amount + 1);
-        
+        vector<unsigned long long> dp(amount + 1, 0);
         dp[0] = 1;
 
         for (int coin : coins) {
