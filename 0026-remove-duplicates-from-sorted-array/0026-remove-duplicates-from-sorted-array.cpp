b96ccb59399821010d9c2adc4eb5ef4e84d72db6
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-06 12:10:25 +0200

    Time: 0 ms (100%), Space: 22.7 MB (49.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,16 +3,24 @@ public:
     int removeDuplicates(vector<int>& nums) {
         vector<int>::size_type n = nums.size();
 
+        if (n <= 1) {
+            return n;
+        }
+
         int write = 1;
         int read = 1;
 
+        while (write < n && nums[write] != nums[write - 1]) {
+            ++write;
+            ++read;
+        }
+
         while (read < n) {
-            if (nums[read] == nums[read - 1]) {
-                ++read;
-                continue;
+            if (nums[read] != nums[read - 1]) {
+                nums[write++] = nums[read];
             }
 
-            nums[write++] = nums[read++];
+            ++read;
         }
 
         return write;
