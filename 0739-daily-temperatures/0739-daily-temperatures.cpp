d2067876bcf80769c46a661ed4d181ad3d40f135
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-06 12:40:30 +0200

    Time: 13 ms (88.57%), Space: 102.8 MB (83.1%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int>::size_type n = temperatures.size();
+        vector<int> answer(n);
+        stack<int> maxIdxStack{};
+
+        for (int i = n - 1; i >= 0; --i) {
+            while (!maxIdxStack.empty() && temperatures[maxIdxStack.top()] <= temperatures[i]) {
+                maxIdxStack.pop();
+            }
+
+            if (maxIdxStack.empty()) {
+                answer[i] = 0;
+                maxIdxStack.push(i);
+                continue;
+            }
+
+            answer[i] = maxIdxStack.top() - i;
+            maxIdxStack.push(i);
+        }
+
+        return answer;
+    }
+};
