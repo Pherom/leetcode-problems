e98b0fda1c059436989fad622225510ac33d8023
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-06 13:18:01 +0200

    Time: 0 ms (100%), Space: 18.6 MB (51.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,23 +4,48 @@ public:
         vector<vector<int>>::size_type m = matrix.size();
         vector<int>::size_type n = matrix[0].size();
 
-        vector<bool> markedRows(m, false);
-        vector<bool> markedCols(n, false);
+        bool includeRow = false;
+        bool includeCol = false;
 
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+            if (matrix[i][0] == 0) {
+                includeCol = true;
+                break;
+            }
+        }
+
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                includeRow = true;
+                break;
+            }
+        }
+
+        // Mark top-left frame of matrix
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
                 if (matrix[i][j] == 0) {
-                    markedCols[j] = true;
-                    markedRows[i] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (markedCols[j] || markedRows[i]) {
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if (includeRow) {
+            std::fill(matrix[0].begin(), matrix[0].end(), 0);
+        }
+
+        if (includeCol) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
             }
         }
     }
