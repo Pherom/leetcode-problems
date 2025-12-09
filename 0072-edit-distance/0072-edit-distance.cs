4f47aebc8b6939c317fbe1883676d928c35a952f
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-09 23:28:45 +0200

    Time: 3 ms (96.41%), Space: 45.1 MB (92.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cs
+++ 0072-edit-distance/0072-edit-distance.cs
@@ -28,29 +28,28 @@ public class Solution {
     public int MinDistance(string word1, string word2) {
         int n1 = word1.Length;
         int n2 = word2.Length;
-        var dp = new int[2, n2 + 1];
+        var dp1 = new int[n2 + 1];
+        var dp2 = new int[n2 + 1];
 
         for (int j = 0; j <= n2; ++j) {
-            dp[1, j] = n2 - j;
+            dp2[j] = n2 - j;
         }
 
         for (int i = n1 - 1; i >= 0; --i) {
-            dp[0, n2] = n1 - i;
+            dp1[n2] = n1 - i;
 
             for (int j = n2 - 1; j >= 0; --j) {
                 if (word1[i] == word2[j]) {
-                    dp[0, j] = dp[1, j + 1];
+                    dp1[j] = dp2[j + 1];
                     continue;
                 }
 
-                dp[0, j] = 1 + Math.Min(dp[1, j], Math.Min(dp[1, j + 1], dp[0, j + 1]));
+                dp1[j] = 1 + Math.Min(dp2[j], Math.Min(dp2[j + 1], dp1[j + 1]));
             }
 
-            for (int j = 0; j <= n2; ++j) {
-                dp[1, j] = dp[0, j];
-            }
+            Array.Copy(dp1, 0, dp2, 0, n2 + 1);
         }
 
-        return dp[1, 0];
+        return dp2[0];
     }
 }
