922dde49d1364dbb8e22a3b8658ad429a0951086
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-09 19:15:49 +0200

    Time: 49 ms (19.34%), Space: 48.3 MB (95.21%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cs
@@ -0,0 +1,54 @@
+public class Solution {
+    public IList<IList<int>> FourSum(int[] nums, int target) {
+        int n = nums.Length;
+        IList<IList<int>> res = new List<IList<int>>();
+        IList<int> curr = new List<int>();
+        Array.Sort(nums);
+
+        void kSum(long target, int k, int start) {
+            if (k == 2) {
+                int left = start;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = nums[left] + nums[right];
+
+                    if (sum == target) {
+                        curr.Add(nums[left]);
+                        curr.Add(nums[right]);
+                        res.Add(new List<int>(curr));
+                        curr.RemoveAt(curr.Count - 1);
+                        curr.RemoveAt(curr.Count - 1);
+
+                        do {
+                            ++left;
+                        } while (left < right && nums[left] == nums[left - 1]);
+
+                        do {
+                            --right;
+                        } while (left < right && nums[right] == nums[right + 1]);
+                    } else if (sum < target) {
+                        ++left;
+                    } else {
+                        --right;
+                    }
+                }
+
+                return;
+            }
+
+            for (int i = start; i < n - k + 1; ++i) {
+                if (i > start && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+
+                curr.Add(nums[i]);
+                kSum(target - nums[i], k - 1, i + 1);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
+
+        kSum(target, 4, 0);
+        return res;
+    }
+}
