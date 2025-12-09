5bfa3bdf7c5207a34dfe8689ca694a7fe808b6df
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-10 01:30:16 +0200

    Time: 10 ms (52.2%), Space: 29.5 MB (51.57%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int TotalNQueens(int n) {
+        var placedQueens = new List<(int R, int C)>();
+
+        int rec(int row) {
+            if (row == n) {
+                return 1;
+            }
+
+            int count = 0;
+
+            for (int col = 0; col < n; ++col) {
+                bool conflict = false;
+
+                foreach (var (otherRow, otherCol) in placedQueens) {
+                    if (otherCol == col || otherCol + otherRow == col + row || otherCol - otherRow == col - row) {
+                        conflict = true;
+                        break;
+                    }
+                }
+
+                if (conflict) {
+                    continue;
+                }
+
+                placedQueens.Add((row, col));
+                count += rec(row + 1);
+                placedQueens.RemoveAt(placedQueens.Count - 1);
+            }
+
+            return count;
+        }
+
+        return rec(0);
+    }
+}
