482a90ed3f4ea5c0fa7516c7c635693e46b3b244
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-09 19:53:07 +0200

    Time: 11 ms (89.42%), Space: 49.2 MB (25.78%) - LeetHub

--- 0018-4sum/0018-4sum.cs
+++ 0018-4sum/0018-4sum.cs
@@ -1,52 +1,53 @@
 public class Solution {
-    private static void kSum(List<List<int>> res, int[] curr, int[] nums, int n, long target, int k, int start, int addedCount) {
-        if (k == 2) {
-            int left = start;
-            int right = n - 1;
-
-            while (left < right) {
-                long sum = nums[left] + nums[right];
-
-                if (sum == target) {
-                    curr[addedCount] = nums[left];
-                    curr[addedCount + 1] = nums[right];
-                    res.Add(new List<int>(curr));
-
-                    do {
-                        ++left;
-                    } while (left < right && nums[left] == nums[left - 1]);
-
-                    do {
-                        --right;
-                    } while (left < right && nums[right] == nums[right + 1]);
-                } else if (sum < target) {
-                    ++left;
-                } else {
-                    --right;
-                }
-            }
-
-            return;
-        }
-
-        for (int i = start; i < n - k + 1; ++i) {
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            curr[addedCount++] = nums[i];
-            kSum(res, curr, nums, n, target - nums[i], k - 1, i + 1, addedCount);
-            --addedCount;
-        }
-    }
-
     public IList<IList<int>> FourSum(int[] nums, int target) {
         int n = nums.Length;
         var res = new List<List<int>>(n);
-        int[] curr = new int[4];
         Array.Sort(nums);
+        long targetLong = target;
 
-        kSum(res, curr, nums, n, target, 4, 0, 0);
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            targetLong -= nums[i];
+
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                targetLong -= nums[j];
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = nums[left] + nums[right];
+
+                    if (sum == targetLong) {
+                        res.Add(new List<int>(4){ nums[i], nums[j], nums[left], nums[right] });
+
+                        do {
+                            ++left;
+                        } while (left < right && nums[left] == nums[left - 1]);
+
+                        do {
+                            --right;
+                        } while (left < right && nums[right] == nums[right + 1]);
+                    } else if (sum < targetLong) {
+                        ++left;
+                    } else {
+                        --right;
+                    }
+                }
+
+                targetLong += nums[j];
+            }
+
+            targetLong += nums[i];
+        }
+
         return res.ToArray();
     }
 }
