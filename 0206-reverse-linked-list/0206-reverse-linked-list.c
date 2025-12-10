6039473d1926bc20e904c4ae68cfed368c6355ed
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-10 18:35:45 +0200

    Time: 0 ms (100%), Space: 10.6 MB (92.47%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+
+    if (dummy == NULL) {
+        return NULL;
+    }
+
+    dummy->next = head;
+    struct ListNode* curr = head;
+
+    while (curr->next != NULL) {
+        struct ListNode* temp = dummy->next;
+        dummy->next = curr->next;
+        curr->next = curr->next->next;
+        dummy->next->next = temp;
+    }
+
+    head = dummy->next;
+    free(dummy);
+    return head;
+}
