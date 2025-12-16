a8493285d2ef2ac14060f672502bb0c8ff0fb175
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-16 23:37:01 +0200

    Time: 0 ms (100%), Space: 14.9 MB (63.87%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -1,58 +1,33 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-static void buildLUT(unsigned char* lut) {
-    lut[0] = 0;
-
-    unsigned char curr = 1;
-
-    while (curr > 0) {
-        unsigned char copy = curr;
-
-        while (copy > 0) {
-            if ((copy & 1) > 0) {
-                ++lut[curr];
-            }
-
-            copy >>= 1;
-        }
-
-        ++curr;
-    }
-}
-
-static unsigned char countBitsByte(unsigned char byte) {
-    static unsigned char lut[1 << CHAR_BIT];
-    static bool lutBuilt = false;
-
-    if (!lutBuilt) {
-        buildLUT(lut);
-        lutBuilt = true;
-    }
-
-    return lut[byte];
-}
-
 int* countBits(int n, int* returnSize) {
     if (returnSize == NULL) {
         return NULL;
     }
 
-    int* result = (int*)calloc(n + 1, sizeof(int));
+    int* result = calloc(n + 1, sizeof(int));;
 
     if (result == NULL) {
         return NULL;
     }
 
     *returnSize = n + 1;
+    unsigned int bound = 1;
 
     result[0] = 0;
-    for (int i = 1; i <= n; ++i) {
-        unsigned int copy = i;
 
-        while (copy > 0) {
-            result[i] += countBitsByte(copy);
-            copy >>= CHAR_BIT;
+    if (n == 0) {
+        return result;
+    }
+
+    result[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        if (i == (bound << 1)) {
+            bound <<= 1;
+            result[i] = 1;
+        } else {
+            result[i] = 1 + result[i - bound];
         }
     }
 
