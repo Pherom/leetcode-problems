f2b9361b1f06e9c63f24db2f6a1277a93c9b70a3
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-16 21:57:00 +0200

    Time: 2 ms (49.04%), Space: 8.4 MB (13.84%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,16 +1,51 @@
+static void buildLUT(unsigned char* lut) {
+    lut[0] = 0;
+
+    unsigned char curr = 1;
+
+    while (curr > 0) {
+        unsigned char copy = curr;
+        unsigned char reversed = 0;
+
+        for (int i = 0; i < CHAR_BIT; ++i) {
+            reversed <<= 1;
+
+            unsigned char bit = copy & 1;
+
+            if (bit > 0) {
+                reversed |= 1;
+            }
+
+            copy >>= 1;
+        }
+
+        lut[curr++] = reversed;
+    }
+}
+
+static unsigned char reverseBitsByte(unsigned char n) {
+    static char lut[1 << CHAR_BIT];
+    static bool lutBuilt = false;
+
+    if (!lutBuilt) {
+        buildLUT(lut);
+        lutBuilt = true;
+    }
+
+    return lut[n];
+}
+
 int reverseBits(int n) {
     int result = 0;
 
-    for (int i = 0; i < sizeof(int) * CHAR_BIT; ++i) {
-        result <<= 1;
+    for (int i = 0; i < sizeof(int); ++i) {
+        result <<= CHAR_BIT;
 
-        unsigned int bit = n & 1;
+        unsigned char rmBReversed = reverseBitsByte(n);
 
-        if (bit > 0) {
-            result |= 1;
-        }
+        result |= (unsigned int)rmBReversed;
 
-        n >>= 1;
+        n >>= CHAR_BIT;
     }
 
     return result;
