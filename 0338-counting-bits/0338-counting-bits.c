c241c37a372d4e62f3d5181c81a61c9ac9a032ca
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-19 13:26:07 +0200

    Time: 0 ms (100%), Space: 14.9 MB (96.7%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -2,27 +2,23 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* countBits(int n, int* returnSize) {
-    if (returnSize == NULL) {
+    int* ans = (int*)malloc(sizeof(int) * (n + 1));
+
+    if (ans == NULL) {
         return NULL;
     }
 
-    int* result = calloc(n + 1, sizeof(int));;
+    ans[0] = 0;
+    unsigned int bound = 1;
 
-    if (result == NULL) {
-        return NULL;
+    for (int i = 1; i <= n; ++i) {
+        if (i == (bound << 1)) {
+            bound <<= 1;
+        }
+
+        ans[i] = 1 + ans[i - bound];
     }
 
     *returnSize = n + 1;
-    unsigned int bound = 1;
-
-    result[0] = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (i == (bound << 1)) {
-            bound = i;
-        }
-        
-        result[i] = 1 + result[i - bound];
-    }
-
-    return result;
+    return ans;
 }
