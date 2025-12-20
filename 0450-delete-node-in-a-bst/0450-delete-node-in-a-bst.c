4c921b798f8681435ff2b817a9401fde5a8748b8
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-20 14:18:51 +0200

    Time: 0 ms (100%), Space: 18.3 MB (50.24%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.c
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.c
@@ -11,34 +11,35 @@ struct TreeNode* deleteNode(struct TreeNode* root, int key) {
         return NULL;
     }
 
-    if (root->val != key) {
+    if (root->val > key) {
         root->left = deleteNode(root->left, key);
+    } else if (root->val < key) {
         root->right = deleteNode(root->right, key);
-        return root;
-    }
-
-    if (root->right == NULL) {
-        struct TreeNode* left = root->left;
-        free(root);
-        return left;
-    }
-
-    struct TreeNode* prev = root;
-    struct TreeNode* curr = root->right;
-
-    while (curr->left != NULL) {
-        prev = curr;
-        curr = curr->left;
-    }
-
-    root->val = curr->val;
-
-    if (prev->right == curr) {
-        prev->right = curr->right;
     } else {
-        prev->left = curr->right;
+        if (root->right == NULL) {
+            struct TreeNode* left = root->left;
+            free(root);
+            return left;
+        }
+
+        struct TreeNode* prev = root;
+        struct TreeNode* curr = root->right;
+
+        while (curr->left != NULL) {
+            prev = curr;
+            curr = curr->left;
+        }
+
+        root->val = curr->val;
+
+        if (prev->right == curr) {
+            prev->right = curr->right;
+        } else {
+            prev->left = curr->right;
+        }
+
+        free(curr);
     }
 
-    free(curr);
     return root;
 }
