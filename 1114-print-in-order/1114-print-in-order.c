a42977a7f28088bbd704595af71c558ed1205595
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-20 16:42:23 +0200

    Time: 5 ms (68.46%), Space: 9 MB (38.85%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.c
@@ -0,0 +1,63 @@
+typedef struct {
+    // User defined data may be declared here.
+    int turn;
+    pthread_mutex_t mut;
+    pthread_cond_t cond;
+} Foo;
+
+// Function Declaration, do not remove
+void printFirst();
+void printSecond();
+void printThird();
+
+Foo* fooCreate() {
+    Foo* obj = (Foo*) malloc(sizeof(Foo));
+    
+    // Initialize user defined data here.
+    obj->turn = 1;
+    pthread_mutex_init(&(obj->mut), NULL);
+    pthread_cond_init(&(obj->cond), NULL);
+
+    return obj;
+}
+
+void first(Foo* obj) {
+    pthread_mutex_lock(&(obj->mut));
+    while (obj->turn != 1) {
+        pthread_cond_wait(&(obj->cond), &(obj->mut));
+    }
+    // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+    obj->turn = 2;
+    pthread_mutex_unlock(&(obj->mut));
+    pthread_cond_broadcast(&(obj->cond));
+}
+
+void second(Foo* obj) {
+    pthread_mutex_lock(&(obj->mut));
+    while (obj->turn != 2) {
+        pthread_cond_wait(&(obj->cond), &(obj->mut));
+    }
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+    obj->turn = 3;
+    pthread_mutex_unlock(&(obj->mut));
+    pthread_cond_broadcast(&(obj->cond));
+}
+
+void third(Foo* obj) {
+    pthread_mutex_lock(&(obj->mut));
+    while (obj->turn != 3) {
+        pthread_cond_wait(&(obj->cond), &(obj->mut));
+    }
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+    obj->turn = 1;
+    pthread_mutex_unlock(&(obj->mut));
+    pthread_cond_broadcast(&(obj->cond));
+}
+
+void fooFree(Foo* obj) {
+    // User defined data may be cleaned up here.
+    
+}
