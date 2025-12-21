70d0d3e9f706f6823069aef25d98a365e1b675be
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-21 10:08:47 +0200

    Time: 0 ms (100%), Space: 15.4 MB (17.63%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,14 @@
+int maxProfit(int* prices, int pricesSize) {
+    int buy = prices[0];
+    int profit = 0;
+
+    for (int i = 1; i < pricesSize; ++i) {
+        if (prices[i] - buy > profit) {
+            profit = prices[i] - buy;
+        } else if (prices[i] < buy) {
+            buy = prices[i];
+        }
+    }
+
+    return profit;
+}
