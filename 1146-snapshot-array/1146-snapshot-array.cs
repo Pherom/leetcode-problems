4086c46367049e784e5c5bf421de0c0a3942056c
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-25 17:44:51 +0200

    Time: 53 ms (83.93%), Space: 126.8 MB (76.79%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.cs
@@ -0,0 +1,64 @@
+public class SnapshotArray {
+
+    List<List<(int Snap, int Val)>> snapsByIndex;
+    private int snapsTaken;
+
+    public SnapshotArray(int length) {
+        snapsByIndex = new List<List<(int Snap, int Val)>>(length);
+        for (int i = 0; i < length; ++i) {
+            snapsByIndex.Add(new List<(int Snap, int Val)>());
+        }
+        snapsTaken = 0;
+    }
+    
+    public void Set(int index, int val) {
+        if (snapsByIndex[index].Count == 0) {
+            snapsByIndex[index].Add((snapsTaken, val));
+        } else {
+            if (snapsByIndex[index].Last().Snap == snapsTaken) {
+                snapsByIndex[index][^1] = (snapsTaken, val);
+            } else {
+                snapsByIndex[index].Add((snapsTaken, val));
+            }
+        }
+    }
+    
+    public int Snap() {
+        return snapsTaken++;
+    }
+    
+    public int Get(int index, int snap_id) {
+        if (snapsByIndex[index] == null) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = snapsByIndex[index].Count - 1;
+        int res = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midSnap = snapsByIndex[index][mid].Snap;
+            int midVal = snapsByIndex[index][mid].Val;
+
+            if (midSnap == snap_id) {
+                return midVal;
+            } else if (midSnap < snap_id) {
+                res = midVal;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return res;
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.Set(index,val);
+ * int param_2 = obj.Snap();
+ * int param_3 = obj.Get(index,snap_id);
+ */
