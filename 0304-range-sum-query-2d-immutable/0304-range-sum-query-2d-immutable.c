a27d3e4e914204e0996551cc83d62847324e6015
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-27 19:01:41 +0200

    Time: 539 ms (5.33%), Space: 77.5 MB (6.67%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.c
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.c
@@ -13,7 +13,10 @@ NumMatrix* numMatrixCreate(int** matrix, int matrixSize, int* matrixColSize) {
         return NULL;
     }
 
-    long long* sumMat = (long long*)malloc(sizeof(long long) * matrixSize * *matrixColSize);
+    int m = matrixSize + 1;
+    int n = *matrixColSize + 1;
+
+    long long* sumMat = (long long*)malloc(sizeof(long long) * m * n);
 
     if (sumMat == NULL) {
         return NULL;
@@ -26,23 +29,34 @@ NumMatrix* numMatrixCreate(int** matrix, int matrixSize, int* matrixColSize) {
         return NULL;
     }
 
-    int m = matrixSize;
-    int n = *matrixColSize;
-
-    sumMat[0] = matrix[0][0];
-
-    for (int j = 1; j < n; ++j) {
-        sumMat[j] = sumMat[j - 1] + matrix[0][j];
-    }
+    memset(sumMat, 0, sizeof(long long) * n);
 
     for (int i = 1; i < m; ++i) {
-        sumMat[i * n] = sumMat[(i - 1) * n] + matrix[i][0];
+        sumMat[i * n] = 0;
     }
 
-    for (int i = 1; i < m; ++i) {
-        for (int j = 1; j < n; ++j) {
-            sumMat[i * n + j] = sumMat[(i - 1) * n + j] + sumMat[i * n + j - 1] - sumMat[(i - 1) * n + j - 1] + matrix[i][j];
+    sumMat[n + 1] = matrix[0][0];
+
+    for (int j = 2; j < n; ++j) {
+        sumMat[n + j] = sumMat[n + j - 1] + matrix[0][j - 1];
+    }
+
+    for (int i = 2; i < m; ++i) {
+        sumMat[i * n + 1] = sumMat[(i - 1) * n + 1] + matrix[i - 1][0];
+    }
+
+    for (int i = 2; i < m; ++i) {
+        for (int j = 2; j < n; ++j) {
+            sumMat[i * n + j] = sumMat[(i - 1) * n + j] + sumMat[i * n + j - 1] - sumMat[(i - 1) * n + j - 1] + matrix[i - 1][j - 1];
         }
+    }
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%d, ", sumMat[i * n + j]);
+        }
+
+        puts("");
     }
 
     result->sumMat = sumMat;
@@ -56,26 +70,15 @@ int numMatrixSumRegion(NumMatrix* obj, int row1, int col1, int row2, int col2) {
         return -1;
     }
 
-    long long top = 0;
-    long long left = 0;
-    long long diag = 0;
     long long* sumMat = obj->sumMat;
     int m = obj->m;
     int n = obj->n;
 
-    if (row1 > 0) {
-        top = sumMat[(row1 - 1) * n + col2];
-    }
+    long long top = sumMat[row1 * n + col2 + 1];
+    long long left = sumMat[(row2 + 1) * n + col1];
+    long long diag = sumMat[row1 * n + col1];
 
-    if (col1 > 0) {
-        left = sumMat[row2 * n + col1 - 1];
-    }
-
-    if (row1 > 0 && col1 > 0) {
-        diag = sumMat[(row1 - 1) * n + col1 - 1];
-    }
-
-    return sumMat[row2 * n + col2] - top - left + diag;
+    return sumMat[(row2 + 1) * n + col2 + 1] - top - left + diag;
 }
 
 void numMatrixFree(NumMatrix* obj) {
