1a4a92334efc945911c8ae3b351636acbed7207c
Pherom <43972513+Pherom@users.noreply.github.com>
2025-12-29 12:34:31 +0200

    Time: 0 ms (100%), Space: 7.7 MB (54.5%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.c
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.c
@@ -1,13 +1,11 @@
 int getSum(int a, int b) {
-    unsigned int ua = a;
-    unsigned int ub = b;
     unsigned int carry = 0;
-    unsigned int res = 0;
+    int res = 0;
     unsigned int mask = 1;
 
     while (mask > 0) {
-        unsigned int aBit = ua & mask;
-        unsigned int bBit = ub & mask;
+        int aBit = a & mask;
+        int bBit = b & mask;
         res |= aBit ^ bBit ^ carry;
         carry = ((aBit & bBit) | (aBit & carry) | (bBit & carry)) << 1;
         mask <<= 1;
