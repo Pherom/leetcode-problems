34e3f673d345055e7100cbf4be8da96fa4e081a9
Pherom <43972513+Pherom@users.noreply.github.com>
2026-01-02 12:19:17 +0200

    Time: 1 ms (96.19%), Space: 30 MB (30.48%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cs
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cs
@@ -1,20 +1,13 @@
 public class Solution {
     public int RangeBitwiseAnd(int left, int right) {
-        uint uleft = (uint)left;
-        uint uright = (uint)right;
-        int res = 0;
-        int mask = 1;
+        int shift = 0;
 
-        while (uleft > 0) {
-            if (uleft % 2 == 1 && uright == uleft) {
-                res |= mask;
-            }
-
-            uright >>= 1;
-            uleft >>= 1;
-            mask <<= 1;
+        while (left < right) {
+            right >>= 1;
+            left >>= 1;
+            ++shift;
         }
 
-        return res;
+        return left << shift;
     }
 }
