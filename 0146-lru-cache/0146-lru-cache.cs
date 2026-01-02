365dd96d3ee1d426b3366eeae7a823d024f5dadc
Pherom <43972513+Pherom@users.noreply.github.com>
2026-01-02 13:04:01 +0200

    Time: 43 ms (28.56%), Space: 174.8 MB (43.82%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cs
@@ -0,0 +1,52 @@
+public class LRUCache {
+
+    private Dictionary<int, LinkedListNode<(int Key, int Value)>> dict;
+    private LinkedList<(int Key, int Value)> list;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        dict = new Dictionary<int, LinkedListNode<(int Key, int Value)>>();
+        list = new LinkedList<(int Key, int Value)>();
+        this.capacity = capacity;
+    }
+    
+    public int Get(int key) {
+        if (dict.ContainsKey(key)) {
+            LinkedListNode<(int Key, int Value)> node = dict[key];
+
+            list.Remove(node);
+            list.AddFirst(node);
+            return node.ValueRef.Value;
+        }
+
+        return -1;
+    }
+    
+    public void Put(int key, int value) {
+        if (dict.ContainsKey(key)) {
+            LinkedListNode<(int Key, int Value)> node = dict[key];
+
+            list.Remove(node);
+            list.AddFirst(node);
+            node.ValueRef = (key, value);
+            return;
+        }
+
+        if (list.Count == capacity) {
+            LinkedListNode<(int Key, int Value)> last = list.Last;
+
+            dict.Remove(last.ValueRef.Key);
+            list.Remove(last);
+        }
+
+        list.AddFirst((key, value));
+        dict.Add(key, list.First);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
