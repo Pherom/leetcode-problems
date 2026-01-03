11f7dfb0cd48cfe80e335efad36f368a83ac668b
Pherom <43972513+Pherom@users.noreply.github.com>
2026-01-03 13:37:24 +0200

    Time: 0 ms (100%), Space: 10.8 MB (29.21%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.c
@@ -0,0 +1,32 @@
+#define MAX(first, second) ( ( first ) > ( second ) ? ( first ) : ( second ) )
+#define MIN(first, second) ( ( first ) < ( second ) ? ( first ) : ( second ) )
+
+int maxSubarraySumCircular(int* nums, int numsSize) {
+    long long globalMax = nums[0];
+    long long globalMin = nums[0];
+    long long max = 0;
+    long long min = 0;
+    long long total = 0;
+
+    for (int i = 0; i < numsSize; ++i) {
+        int num = nums[i];
+        long long includedMax = max + num;
+        long long includedMin = min + num;
+
+        max = MAX(num, includedMax);
+        min = MIN(num, includedMin);
+
+        globalMax = MAX(globalMax, max);
+        globalMin = MIN(globalMin, min);
+
+        total += num;
+    }
+
+    long long altMax = total - globalMin;
+
+    if (globalMin == total) {
+        return globalMax;
+    }
+
+    return MAX(globalMax, altMax);
+}
