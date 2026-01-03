f8dc3464c3a8d6ea3eaa3f39cca5b884fd0a458a
Pherom <43972513+Pherom@users.noreply.github.com>
2026-01-03 12:53:54 +0200

    Time: 15 ms (70.11%), Space: 24.3 MB (91.59%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> ordToAnagrams{};
+
+        for (string const& str : strs) {
+            string sortedStr{str};
+            std::sort(sortedStr.begin(), sortedStr.end());
+
+            auto it = ordToAnagrams.find(sortedStr);
+
+            if (it == ordToAnagrams.end()) {
+                ordToAnagrams.insert({sortedStr, vector<string>{}});
+            }
+
+            ordToAnagrams[sortedStr].push_back(str);
+        }
+
+        vector<vector<string>> result{};
+        result.reserve(strs.size());
+
+        for (auto const& p : ordToAnagrams) {
+            result.push_back(std::move(p.second));
+        }
+
+        return result;
+    }
+};
