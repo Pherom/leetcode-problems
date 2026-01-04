e9b2f8a9a0859bdd85b21f1b75b78cef0348d795
Pherom <43972513+Pherom@users.noreply.github.com>
2026-01-04 14:55:40 +0200

    Time: 118 ms (18.31%), Space: 44.2 MB (28.87%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cs
+++ 1114-print-in-order/1114-print-in-order.cs
@@ -2,45 +2,30 @@ using System.Threading;
 
 public class Foo {
 
-    private readonly object lck;
-    private int counter = 1;
+    private AutoResetEvent firstEvent;
+    private AutoResetEvent secondEvent;
 
     public Foo() {
-        lck = new object();
+        firstEvent = new AutoResetEvent(false);
+        secondEvent = new AutoResetEvent(false);
     }
 
     public void First(Action printFirst) {
-        lock (lck) {
-            // printFirst() outputs "first". Do not change or remove this line.
-            printFirst();
-            ++counter;
-
-            Monitor.PulseAll(lck);
-        }
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        firstEvent.Set();
     }
 
     public void Second(Action printSecond) {
-        lock (lck) {
-            while (counter != 2) {
-                Monitor.Wait(lck);
-            }
-
-            // printSecond() outputs "second". Do not change or remove this line.
-            printSecond();
-            ++counter;
-
-            Monitor.PulseAll(lck);
-        }
+        firstEvent.WaitOne();
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        secondEvent.Set();
     }
 
     public void Third(Action printThird) {
-        lock (lck) {
-            while (counter != 3) {
-                Monitor.Wait(lck);
-            }
-
-            // printThird() outputs "third". Do not change or remove this line.
-            printThird();
-        }
+        secondEvent.WaitOne();
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
     }
 }
