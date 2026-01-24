58d3d7c2e2a4f9354dca765899db294e02a30e1f
Pherom <43972513+Pherom@users.noreply.github.com>
2026-01-24 14:52:02 +0200

    Time: 0 ms (100%), Space: 8.4 MB (15.41%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,34 +1,31 @@
-static void buildLUT(unsigned char* lut) {
+void buildLut(unsigned char* lut) {
     lut[0] = 0;
 
     unsigned char curr = 1;
 
     while (curr > 0) {
+        unsigned char res = 0;
         unsigned char copy = curr;
-        unsigned char reversed = 0;
+        int leftShift = CHAR_BIT;
 
-        for (int i = 0; i < CHAR_BIT; ++i) {
-            reversed <<= 1;
-
-            unsigned char bit = copy & 1;
-
-            if (bit > 0) {
-                reversed |= 1;
-            }
-
+        while (copy > 0) {
+            res <<= 1;
+            res |= (copy & 1);
             copy >>= 1;
+            --leftShift;
         }
 
-        lut[curr++] = reversed;
+        res <<= leftShift;
+        lut[curr++] = res;
     }
 }
 
-static unsigned char reverseBitsByte(unsigned char n) {
-    static char lut[1 << CHAR_BIT];
+unsigned char reverseBitsByte(unsigned char n) {
     static bool lutBuilt = false;
+    static unsigned char lut[1 << CHAR_BIT];
 
     if (!lutBuilt) {
-        buildLUT(lut);
+        buildLut(lut);
         lutBuilt = true;
     }
 
@@ -36,17 +33,21 @@ static unsigned char reverseBitsByte(unsigned char n) {
 }
 
 int reverseBits(int n) {
-    int result = 0;
-
-    for (int i = 0; i < sizeof(int); ++i) {
-        result <<= CHAR_BIT;
-
-        unsigned char rmBReversed = reverseBitsByte(n);
-
-        result |= (unsigned int)rmBReversed;
-
-        n >>= CHAR_BIT;
+    if (n == 0) {
+        return 0;
     }
 
-    return result;
+    unsigned int res = 0;
+    unsigned int un = n;
+    int leftShift = CHAR_BIT * sizeof(unsigned int);
+
+    while (un > 0) {
+        res <<= CHAR_BIT;
+        res |= reverseBitsByte(un);
+        un >>= CHAR_BIT;
+        leftShift -= CHAR_BIT;
+    }
+
+    res <<= leftShift;
+    return res;
 }
